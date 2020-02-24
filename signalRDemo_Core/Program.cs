da3d29edba9ef2efbf600ffea933e96aa7d6bbff
吴国宏 <303295469@qq.com>
2020-02-24 23:14:03 +0800

    添加项目文件。

---
+++ signalRDemo_Core/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace signalRDemo_Core
+{
+    class Program
+    {
+        static HubConnection connection;
+        static void Main(string[] args)
+        {
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44391/ChatHub")
+                .Build();
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+            Connect();
+            Console.ReadKey();
+            Send();
+            Console.ReadKey();
+        }
+        private static async void Connect()
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                    var newMessage = $"{user}: {message}";
+                    Console.WriteLine(newMessage);
+            });
+            try
+            {
+                await connection.StartAsync();
+                Console.WriteLine("Connection started");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static async void Send()
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage",
+                    "gavin", "111");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
